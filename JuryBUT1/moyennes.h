5e79ebccc41cf3216db22330f7bf98b884000b10
Hylionx <39769974+hylionx@users.noreply.github.com>
2025-03-23 01:03:06 +0100

    Add files via upload

---
+++ JuryBUT1/moyennes.h
@@ -0,0 +1,18 @@
+﻿#include <stdlib.h> 
+#include <stdio.h>
+#include <string.h>  
+
+typedef struct {
+    char prenom[100];
+    char nom[100];
+    float *moyennes;  // Tableau dynamique des moyennes
+} moyEtu;
+
+typedef struct {
+    int nb_etudiants;
+    int nb_UE;         
+    moyEtu *etudiants; // Tableau dynamique des étudiants
+} moyPromo;
+
+
+void lire_moyennes_allocDyn(FILE *nom_fichier, moyPromo *les_moyennes );
